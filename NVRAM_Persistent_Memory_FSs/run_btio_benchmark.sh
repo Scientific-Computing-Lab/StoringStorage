c3c0a01571306ec7edf523c2f373f03e36eb542c
Yehonatan Fridman <yehonatan123f@gmail.com>
2021-09-05 11:58:39 +0300

    Update run_btio_benchmark.sh

--- NVRAM_Persistent_Memory_FSs/run_btio_benchmark.sh
+++ NVRAM_Persistent_Memory_FSs/run_btio_benchmark.sh
@@ -9,6 +9,7 @@ do
 	sync; echo 1 > /proc/sys/vm/drop_caches
 	sed -i "1s/.*/w                 # IO mode:    w for write, r for read/" inputbt.data
         sed -i "2s/.*/0                 # IO method:  0 for MPI collective IO, 1 for MPI_independent IO, 2 for PnetCDF blocking IO, 3 for PnetCDF nonblocking IO/" inputbt.data
+	sed -i "3s/.*/1                  # number of time steps/" inputbt.data
 	sed -i "4s/.*/$i $i $i           # grid_points(1), grid_points(2), grid_points(3)/" inputbt.data
 	mpiexec -n 4 ./btio inputbt.data >> result_$i-write_mpi_collective.txt 
         echo "done write " $i
@@ -27,6 +28,7 @@ do
 	sync; echo 1 > /proc/sys/vm/drop_caches
 	sed -i "1s/.*/w                 # IO mode:    w for write, r for read/" inputbt.data
         sed -i "2s/.*/1                 # IO method:  0 for MPI collective IO, 1 for MPI_independent IO, 2 for PnetCDF blocking IO, 3 for PnetCDF nonblocking IO/" inputbt.data
+	sed -i "3s/.*/1                  # number of time steps/" inputbt.data
 	sed -i "4s/.*/$i $i $i           # grid_points(1), grid_points(2), grid_points(3)/" inputbt.data
 	mpiexec -n 4 ./btio inputbt.data >> result_$i-write_mpi_independent.txt 
         echo "done write " $i
